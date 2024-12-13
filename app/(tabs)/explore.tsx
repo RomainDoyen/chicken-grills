98069a8083ef616b0c509d068f4093ff29baa7e1
Design And Creation <romain.doyen97@gmail.com>
2024-12-13 11:30:07 +0400

    Integrate a map

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,10 +1,18 @@
-import { StyleSheet, Image, Platform } from 'react-native';
-import { View } from 'react-native-reanimated/lib/typescript/Animated';
+import { StyleSheet, Image, Platform, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-    
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: -21.12165459276416,
+          longitude: 55.54070004999999,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+      />
     </View>
   );
 }
@@ -13,4 +21,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  map: {
+    height: '100%',
+    width: '100%',
+  }
 });
