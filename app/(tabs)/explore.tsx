7d8241b19e20af29bd4ec7c35de41a9bb664cd98
Design And Creation <romain.doyen97@gmail.com>
2024-12-13 11:33:55 +0400

    Store region coordinates in state

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,17 +1,21 @@
 import { StyleSheet, Image, Platform, View } from 'react-native';
+import React, { useState } from 'react';
 import MapView from 'react-native-maps';
 
 export default function TabTwoScreen() {
+
+  const [region, setRegion] = useState({
+    latitude: -21.12165459276416,
+    longitude: 55.54070004999999,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.05,
+  });
+
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
-        initialRegion={{
-          latitude: -21.12165459276416,
-          longitude: 55.54070004999999,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05,
-        }}
+        initialRegion={region}
       />
     </View>
   );
