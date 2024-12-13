feec0fb2b0edcb7ddc387723f01743234a1f8c85
Design And Creation <romain.doyen97@gmail.com>
2024-12-13 11:50:10 +0400

    Add several custom markers to the map

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Image, Platform, View } from 'react-native';
+import { StyleSheet, Image, View } from 'react-native';
 import React, { useState } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 
@@ -11,17 +11,48 @@ export default function TabTwoScreen() {
     longitudeDelta: 0.05,
   });
 
+  const sellers = [
+    {
+      name: 'Poulet Grillé 1',
+      latitude: -20.9606333,
+      longitude: 55.6492731,
+      description: 'Le meilleur poulet grillé de la ville !',
+    },
+    {
+      name: 'Poulet Grillé 2',
+      latitude: -21.1277445,
+      longitude: 55.7922999,
+      description: 'Le meilleur poulet grillé de la ville !',
+    },
+    {
+      name: 'Poulet Grillé 3',
+      latitude: -21.0006099,
+      longitude: 55.2771585,
+      description: 'Le meilleur poulet grillé de la ville !',
+    }
+  ];
+
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
         initialRegion={region}
       >
-        <Marker
+        {sellers.map((seller, index) => (
+          <Marker
+            key={index}
+            coordinate={{ latitude: seller.latitude, longitude: seller.longitude }}
+            title={seller.name}
+            description={seller.description}
+          >
+            <Image source={require('@/assets/images/chicken-marker.png')} style={{ width: 36, height: 36 }} />
+          </Marker>
+        ))}
+        {/* <Marker
           coordinate={{ latitude: -20.9606333, longitude: 55.6492731 }}
           title="Poulet Grillé 1"
           description="Le meilleur poulet grillé de la ville !"
-        />
+        /> */}
       </MapView>
     </View>
   );
